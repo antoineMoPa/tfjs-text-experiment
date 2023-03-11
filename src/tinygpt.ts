e5ec56545a1713a4f732489fcdcdf8f38764392d
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2023-03-11 17:16:02 -0500

    tweak

--- src/tinygpt.ts
+++ src/tinygpt.ts
@@ -451,7 +451,7 @@ export async function buildModel(
                 output = tf.layers.timeDistributed({
                     layer:
                     tf.layers.dense({
-                        units: 260,
+                        units: 270,
                         activation: 'relu',
                         kernelInitializer: tf.initializers.randomUniform({
                             minval: -0.01,
@@ -465,7 +465,7 @@ export async function buildModel(
             dense();
 
             output = tf.layers.lstm({
-                units: 270,
+                units: 280,
                 activation: 'relu',
                 returnSequences: true,
                 kernelInitializer: tf.initializers.randomUniform({
@@ -495,7 +495,7 @@ export async function buildModel(
     layerOutput = tf.layers.timeDistributed({
         layer:
         tf.layers.dense({
-            units: 340,
+            units: 370,
             activation: 'relu',
             kernelInitializer: tf.initializers.randomUniform({ minval: -0.15, maxval: 0.15 }),
             biasInitializer: tf.initializers.constant({ value: -0.01 }),
