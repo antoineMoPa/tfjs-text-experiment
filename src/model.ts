e2cc5caef2c924f7e59b5475ba897eef3a4dd5ae
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2023-03-19 00:39:01 -0400

    Cleanup

--- src/model.ts
+++ src/model.ts
@@ -16,52 +16,6 @@ import { FocusLayer } from './customLayers/focusLayer';
 
 export const CORPUS_PATH = "data/corpus";
 
-class SliceLayer extends tf.layers.Layer {
-    sliceStart: number[];
-    sliceSize: number[];
-
-    constructor(config: { sliceStart: number[]; sliceSize: number[] } & any) {
-        super(config);
-        this.sliceStart = config.sliceStart
-        this.sliceSize = config.sliceSize;
-    }
-
-    computeOutputShape(inputShape) {
-        return [inputShape[0], this.sliceSize[1], this.sliceSize[2]]
-    }
-
-    call(input) {
-        return tf.tidy(() => {
-            return tf.slice(
-                input[0],
-                this.sliceStart,
-                this.sliceSize
-            );
-            // const k = tf.pow(this.x.read(), this.alpha);
-            // return tf.mul(input[0], k);
-        });
-    }
-
-    /**
-     * getConfig() generates the JSON object that is used
-     * when saving and loading the custom layer object.
-     */
-    getConfig() {
-        const config = super.getConfig();
-
-        config.sliceStart = this.sliceStart;
-        config.sliceSize = this.sliceSize;
-
-        return config;
-    }
-
-    static get className() {
-        return 'SliceLayer';
-    }
-}
-
-tf.serialization.registerClass(SliceLayer);
-
 type WordIndexCache = LRU<string, tf.Tensor>;
 
 export const tokenize = (text: string) => {
